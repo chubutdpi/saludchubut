d7f02d6822c8aee57242be12c8cc53f4257d9c50
Silvio Geoffroy <sageoffroy@gmail.com>
2022-04-05 09:59:29 -0300

    configure locales

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ module Saludchubut
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.time_zone = 'Buenos Aires'
+    
+    config.i18n.default_locale = :es
+    I18n.available_locales = [:es]
+
+    config.generators do |g|
+        g.stylesheets false
+    end
+    config.action_controller.default_protect_from_forgery = false
+    config.serve_static_assets = true
+
+    config.generators.helper = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
